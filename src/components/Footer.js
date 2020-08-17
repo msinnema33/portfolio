b5a313f07c4a2f527128d2d78caa747290eec850
Mike Sinnema <54330659+msinnema33@users.noreply.github.com>
2020-08-17 15:37:27 -0700

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -21,7 +21,7 @@ export default class Footer extends Component {
           </ul>
           
         </div>
-        <div id="go-top"><a className="smoothscroll" title="Back to Top" href="#home"><i className="icon-up-open" /></a></div>
+        <div id="go-top"><a className="smoothscroll" title="Back to the Top" href="#home"><i className="icon-up-open" /></a></div>
       </div>
     </footer>
     );
